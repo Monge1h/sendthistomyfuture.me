163028f5e98a168f8a6df4d7607c8951a890bf64
Jorge Monge <jlo4321jl@gmail.com>
2022-03-12 23:46:23 -0600

    ✨ Add config module

--- backend/sendthistomyfuture-me/src/app.module.ts
+++ backend/sendthistomyfuture-me/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
