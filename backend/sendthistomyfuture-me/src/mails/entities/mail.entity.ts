18a5ad2cbc80220567c92a9ea1641b961f1d037b
Jorge Monge <jlo4321jl@gmail.com>
2022-03-13 22:10:21 -0600

    ✨ Add mail entity

--- backend/sendthistomyfuture-me/src/mails/entities/mail.entity.ts
+++ backend/sendthistomyfuture-me/src/mails/entities/mail.entity.ts
@@ -1,1 +1,22 @@
-export class Mail {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Mail extends Document {
+  @Prop({ required: true })
+  mail: string;
+
+  @Prop({ required: true })
+  body: string;
+
+  @Prop({required: true})
+  send_date: string;
+
+  @Prop({ required: true, default: false })
+  verifed: boolean;
+
+  @Prop({ required: true })
+  verification_code: string;
+}
+
+export const MailSchema = SchemaFactory.createForClass(Mail);
