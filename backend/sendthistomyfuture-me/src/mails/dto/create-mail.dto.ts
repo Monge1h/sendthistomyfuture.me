ceb13f23b2147f6bedcc255441b6a19b4593136b
Jorge Monge <jlo4321jl@gmail.com>
2022-03-13 22:11:44 -0600

    ✨ Add validation to mail dto

--- backend/sendthistomyfuture-me/src/mails/dto/create-mail.dto.ts
+++ backend/sendthistomyfuture-me/src/mails/dto/create-mail.dto.ts
@@ -1,1 +1,18 @@
-export class CreateMailDto {}
+import { IsString, IsNotEmpty, IsBoolean, IsDateString, IsEmail } from 'class-validator'
+export class CreateMailDto {
+	@IsString()
+	@IsEmail()
+	@IsNotEmpty()
+	mail: string;
+
+	@IsString()
+	@IsNotEmpty()
+	body: string;
+
+	@IsDateString()
+	send_date: string;
+
+	@IsBoolean()
+	@IsNotEmpty()
+	random_date: boolean;
+}
