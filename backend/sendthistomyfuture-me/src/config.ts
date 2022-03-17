007d767561481f7ff736017ee7a5b2f5a2638e05
Jorge Monge <jlo4321jl@gmail.com>
2022-03-16 20:00:31 -0600

    🔧 Add email variables to config.ts

--- backend/sendthistomyfuture-me/src/config.ts
+++ backend/sendthistomyfuture-me/src/config.ts
@@ -6,5 +6,8 @@ export default registerAs('config', () => {
       mongo_uri: process.env.MONGO_URI,
     },
     secret_key: process.env.SECRET_KEY,
+    email_service: process.env.EMAIL_SERVICE,
+    email_user: process.env.EMAIL_USER,
+    email_password: process.env.EMAIL_PASSWORD,
   };
 });
