3459f98c3f7d6c2ba92148e5480e91d4c55a84bb
Jorge Monge <jlo4321jl@gmail.com>
2022-03-16 21:10:58 -0600

    ✨ Send verification email

--- backend/sendthistomyfuture-me/src/mails/mails.service.ts
+++ backend/sendthistomyfuture-me/src/mails/mails.service.ts
@@ -1,22 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { MailerService } from '@nestjs-modules/mailer';
+import { ConfigService } from '@nestjs/config';
+
 import { CreateMailDto } from './dto/create-mail.dto';
 import { Mail } from './entities/mail.entity';
-import { encrypt } from '../helper/crypto';
+import { encrypt, hashUrl } from '../helper/crypto';
 
 @Injectable()
 export class MailsService {
-  constructor(@InjectModel(Mail.name) private mailModel: Model<Mail>) {}
+  constructor(
+    @InjectModel(Mail.name) private mailModel: Model<Mail>,
+    private readonly mailerService: MailerService,
+    private configService: ConfigService,
+  ) {}
 
   create({ body, mail, send_date, random_date }: CreateMailDto) {
+    let verification_code = hashUrl();
+
+    this.mailerService.sendMail({
+      to: mail,
+      subject: 'Email verification || sendthistomyfuture.me',
+      text: `${this.configService.get<string>(
+        'CLIENT_URL',
+      )}/verification/${verification_code}`,
+    });
     mail = encrypt(mail).content;
     body = encrypt(body).content;
     const newMail = new this.mailModel({
       mail,
       body,
       send_date,
-      verification_code: '23',
+      verification_code,
     });
     return newMail.save();
   }
