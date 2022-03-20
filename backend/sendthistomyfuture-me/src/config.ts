8b4f018f9824306d1da56328351e81cd361b92c2
Jorge Monge <jlo4321jl@gmail.com>
2022-03-20 17:05:26 -0600

    🔧 Add database .env variables to config.ts

--- backend/sendthistomyfuture-me/src/config.ts
+++ backend/sendthistomyfuture-me/src/config.ts
@@ -3,7 +3,12 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('config', () => {
   return { 
     database: {
-      mongo_uri: process.env.MONGO_URI,
+      dbName: process.env.MONGO_DB,
+      user: process.env.MONGO_INITDB_ROOT_USERNAME,
+      password: process.env.MONGO_INITDB_ROOT_PASSWORD,
+      port: parseInt(process.env.MONGO_PORT, 10),
+      host: process.env.MONGO_HOST,
+      connection: process.env.MONGO_CONNECTION,
     },
     secret_key: process.env.SECRET_KEY,
     email_service: process.env.EMAIL_SERVICE,
