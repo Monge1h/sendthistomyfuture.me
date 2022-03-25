602be3e10a4d0a73a2e9c628ec052d0f32dc89b6
Jorge Monge <jlo4321jl@gmail.com>
2022-03-25 13:01:09 -0600

    💚 Change baseURL

--- frontend/sendthistomyfuture-me/helper/axiosClient.tsx
+++ frontend/sendthistomyfuture-me/helper/axiosClient.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://api.sendthistomyfuture.me/api",
+  baseURL: "https://api.sendthistomyfuture.me/api",
 });
 export { axiosClient };
