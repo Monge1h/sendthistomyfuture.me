2410e974e27dd87ceba234a6554e1fe322688847
Jorge Monge <jlo4321jl@gmail.com>
2022-04-08 20:47:25 -0600

    🔧 Enable cors

--- backend/sendthistomyfuture-me/src/main.ts
+++ backend/sendthistomyfuture-me/src/main.ts
@@ -5,7 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe())
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
