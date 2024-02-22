47c4a69745da1e0ec8c06006aefcd2e79ff082e5
Jorge Monge <jlo4321jl@gmail.com>
2024-02-22 10:25:06 -0600

    🧱 Add required providers to main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,9 +5,22 @@ provider "aws" {
 terraform {
   required_version = ">= 0.12.0"
   required_providers {
-	aws = {
-	  source  = "hashicorp/aws"
-	  version = "5.34.0"
-	}
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.34.0"
+    }
+
+    archive = {
+      source  = "hashicorp/archive"
+      version = "2.2.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.1.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.1.0"
+    }
   }
 }
